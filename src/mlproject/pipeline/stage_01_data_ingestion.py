acd4e1d95979be4dec41f68dc578eea26a059518
MA-Hannan426 <ma.hannan230@gmail.com>
2025-10-19 17:50:12 +0500

    data ingesttion stage added

---
+++ src/mlproject/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,31 @@
+from mlproject.config.configuration import ConfigurationManager
+from mlproject.components.data_ingestion import DataIngestion
+from mlproject import logger
+
+STAGE_NAME = "Data Ingestion Stage"
+
+class DataIngestionPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_data()
+        data_ingestion.extract_zip_file()
+       
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> Stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionPipeline()
+        obj.main()
+        logger.info(f">>>>>> Stage {STAGE_NAME} completed <<<<<<\n\n")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+        
+
